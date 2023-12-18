b221288695d738166a9b13fbbb957f367fb18ff0
RuxandraRambet <ruxandraramb@gmail.com>
2023-12-18 22:00:59 +0100

    feat: styled main and cards fix: changed logo feat: added flip-card on hover

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -23,28 +23,28 @@ export default {
 <template>
   <main>
     <!-- Movies area-->
-    <div v-if="store.movies.length">
-      <h2>Movies</h2>
-      <ul>
+    <div v-if="store.movies.length" class="container">
+      <h2 class="display-6 fw-semibold text-light p-4">Movies</h2>
+      <ul class="d-flex flex-wrap">
         <li v-for="movie in store.movies">
           <!-- movie card -->
           <Card :poster="store.apiConfig.urlMoviePoster + movie.poster_path" :title="movie.title"
             :originalTitle="movie.originale_title" :language="movie.original_language"
-            :vote="getVote(movie.vote_average)" />
+            :vote="getVote(movie.vote_average)" :overview="movie.overview"/>
         </li>
       </ul>
     </div>
     <!-- Movies area-->
 
     <!-- Series area -->
-    <div v-if="store.series.length">
-      <h2>TV Series</h2>
-      <ul>
+    <div v-if="store.series.length" class="container">
+      <h2 class="display-6 fw-semibold text-light p-4">TV Series</h2>
+      <ul class="d-flex flex-wrap">
         <li v-for="serie in store.series">
           <!-- serie card -->
           <Card :poster="store.apiConfig.urlMoviePoster + serie.poster_path" :title="serie.name"
             :originalTitle="serie.originale_name" :language="serie.original_language"
-            :vote="getVote(serie.vote_average)" />
+            :vote="getVote(serie.vote_average)" :overview="serie.overview"/>
         </li>
       </ul>
     </div>
@@ -52,4 +52,9 @@ export default {
   </main>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
+main {
+  background-color: $primarybgcolor;
+}
+</style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,41 +1,88 @@
 <script>
 export default {
     name: 'Card',
-    props: ['poster', 'title', 'originalTitle', 'language', 'vote'],
+    props: ['poster', 'title', 'originalTitle', 'language', 'vote', 'overview'],
 }
 </script>
 
 <template>
-    <div class="card-container">
-        <div class="movie-poster">
-            <img :src="poster" :alt="title">
-        </div>
-        <div class="movie-info">
-            <div class="title">
-                <h3>{{ title }}</h3>
+    <div class="flip-card">
+        <div class="flip-card-inner">
+            <div class="flip-card-front">
+                <img :src="poster" :alt="title" class="img-fluid h-100 w-100 object-fit-cover">
             </div>
-            <div class="original-title">
-                <h4>{{ originalTitle }}</h4>
+            <div class="flip-card-back p-3">
+                <div class="title">
+                    <h3 class="fs-6 text-secondary">{{ title }}</h3>
+                </div>
+                <div class="original-title">
+                    <h3 class="fs-6 text-secondary">{{ originalTitle }}</h3>
+                </div>
+                <div class="language text-secondary">
+                    <p v-if="language === 'it'">
+                        <img src="../assets/img/itflag.png" alt="It Flag">
+                    </p>
+                    <p v-else-if="language === 'en'">
+                        <img src="../assets/img/ukflag.png" alt="UK Flag">
+                    </p>
+                    <p else>
+                        {{ language }}
+                    </p>
+                </div>
+                <div class="vote">
+                    <p>
+                        <font-awesome-icon icon="fa-solid fa-star" v-for="n in vote" class="text-warning"/>
+                        <font-awesome-icon icon="fa-regular fa-star" v-for="n in 5 - vote" />
+                    </p>
+                </div>
+                <div class="overview">
+                    <p class="fs-6 text-secondary">{{ overview }}</p>
+                </div>
             </div>
-            <div class="language">
-                <p v-if="language === 'it'">
-                    <img src="../assets/img/itflag.png" alt="It Flag">
-                </p>
-                <p v-else-if="language === 'en'">
-                    <img src="../assets/img/ukflag.png" alt="UK Flag">
-                </p>
-                <p else>
-                    {{ language }}
-                </p>
-            </div>
-            <div class="vote">
-                <p>
-                    <font-awesome-icon icon="fa-solid fa-star" v-for="n in vote" />
-                    <font-awesome-icon icon="fa-regular fa-star" v-for="n in 5 - vote" />
-                </p>
-            </div>
+
         </div>
     </div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
+
+.flip-card {
+    background-color: transparent;
+    width: 200px;
+    height: 250px;
+    border: 5px solid $primarybgcolor;
+    perspective: 1000px;
+}
+
+.flip-card-inner {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    transition: transform 0.8s;
+    transform-style: preserve-3d;
+}
+
+.flip-card:hover .flip-card-inner {
+    transform: rotateY(180deg);
+}
+
+.flip-card-front,
+.flip-card-back {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+}
+
+.flip-card-back {
+    background-color: $headerbgcolor;
+    transform: rotateY(180deg);
+    overflow-y: auto;
+}
+.language img {
+    width: 25px;
+}
+</style>
